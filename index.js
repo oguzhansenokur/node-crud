b2516eea6a9717bbc656f91b9fcb317b0d97970b
oguzhansenokur <oguzhansenokur61@gmail.com>
2024-02-24 21:43:52 +0300

    ejs implementation

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const express = require('express')
-const app = new express()
-app.use(express.static('public'))
+const express = require('express');
+
+const app = new express();
+app.use(express.static('public'));
 const path = require('path');
-app.get('/',(req,res)=>{ res.sendFile(path.resolve(__dirname,'pages/index.html'))
-})
-app.get('/about',(req,res)=>{ res.sendFile(path.resolve(__dirname,'pages/about.html'))
-})
-app.get('/contact',(req,res)=>{ res.sendFile(path.resolve(__dirname,'pages/contact.html'))
-})
-app.get('/post',(req,res)=>{ res.sendFile(path.resolve(__dirname,'pages/post.html'))
-})
 
-app.listen(3006, ()=>{
-console.log('App listening on port 3006')
-})
+const ejs = require('ejs');
 
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+app.listen(3006, () => {
+  console.log('App listening on port 3006');
+});
