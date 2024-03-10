fe22cd7162b988560798f25ee7774165846620c0
oguzhansenokur <oguzhansenokur61@gmail.com>
2024-03-10 16:57:54 +0300

    linked blogposts to their users by populate on id

--- controllers/getPostController.js
+++ controllers/getPostController.js
@@ -1,6 +1,6 @@
 const BlogPost = require('../models/BlogPost');
 
 module.exports = async (req, res) => {
-  const blogPost = await BlogPost.find({ _id: req.params.id });
+  const blogPost = await BlogPost.find({ _id: req.params.id }).populate('userid');
   res.render('post', { blogPost });
 };
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,6 @@
 const BlogPost = require('../models/BlogPost');
 
 module.exports = async (req, res) => {
-  const blogPosts = await BlogPost.find({});
+  const blogPosts = await BlogPost.find({}).populate('userid');
   res.render('index', { blogPosts });
 };
